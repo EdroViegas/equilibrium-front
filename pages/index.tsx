7e53b933646ac75ff6ff99f9d3fc3c5ea31d85d2
EdroViegas <pedropombalviegas@gmail.com>
2021-11-16 14:11:23 +0000

    Test CI

--- pages/index.tsx
+++ pages/index.tsx
@@ -51,7 +51,7 @@ export default function Home() {
   }
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
+    <div className="min-h-screen flex items-center justify-center bg-red-700 py-12 px-4 sm:px-6 lg:px-8">
       <Head>
         <title>Acessar a conta </title>
       </Head>
