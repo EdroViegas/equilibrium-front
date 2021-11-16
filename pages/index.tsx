7dac7d000a91976947570e89efe715c7b5713f8a
EdroViegas <pedropombalviegas@gmail.com>
2021-11-16 14:27:17 +0000

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -51,7 +51,7 @@ export default function Home() {
   }
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-indigo-300 py-12 px-4 sm:px-6 lg:px-8">
+    <div className="min-h-screen flex items-center justify-center bg-indigo-50 py-12 px-4 sm:px-6 lg:px-8">
       <Head>
         <title>Acessar a conta </title>
       </Head>
